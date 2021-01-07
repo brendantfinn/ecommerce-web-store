8490c3d633d272c871efac3ba0983aaff952b961
brendantfinn <brendantfinn@gmail.com>
2021-01-06 22:35:53 -0600

    cart buttons functional

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,7 +4,7 @@ import useStyles from './styles'
 import CardItem from './CartItem/CartItem'
 import {  Link } from "react-router-dom";
 
-const Cart = ({ cart }) => {
+const Cart = ({ cart, handleAddToCart, handleUpdateCartQty, handleRemoveFromCart, handleEmptyCart}) => {
     
     
     const classes = useStyles();
@@ -20,13 +20,13 @@ const Cart = ({ cart }) => {
             <Grid container spacing={3}>
                 {cart.line_items.map((item)=>(
                     <Grid item xs={12} sm={4} key={item.id}>
-                        <div><CardItem item={item}/></div>
+                        <div><CardItem item={item} onUpdateCartQty={handleUpdateCartQty} onRemoveFromCart={handleRemoveFromCart}/></div>
                     </Grid>
                 ))}
             </Grid>
             <div className={classes.cardDetails}>
                     <Typography variant="h4">Subtotal: {cart.subtotal.formatted_with_symbol}</Typography>
-                    <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary">Empty Cart</Button>
+                    <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary" onClick={handleEmptyCart}>Empty Cart</Button>
                     <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="primary">Checkout</Button>
             </div>
         </>
