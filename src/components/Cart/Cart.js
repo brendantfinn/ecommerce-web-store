0d4850cf02deaebc722924d59357d83c5a6a01ce
brendantfinn <brendantfinn@gmail.com>
2021-01-06 21:00:56 -0600

    react router added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,14 +2,19 @@ import React from 'react'
 import {Container, Typography, Button, Grid } from '@material-ui/core'
 import useStyles from './styles'
 import CardItem from './CartItem/CartItem'
+import {  Link } from "react-router-dom";
+
 const Cart = ({ cart }) => {
     
     
     const classes = useStyles();
 
     const EmptyCart = () => (
-        <Typography variant="subtitle1">You have no items in your shopping cart.</Typography>
-    )
+        <Typography variant="subtitle1">You have no items in your shopping cart.
+            <Link to="/" className={classes.link}>Start adding some!</Link>
+        </Typography>
+    
+        )
     const FilledCart = () => (
         <>
             <Grid container spacing={3}>
